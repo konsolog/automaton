d7ca9bf64d3475b4a7998f884880afeb9da746da
rekkord <geraldjamestan@gmail.com>
2025-06-07 11:16:16 +0800

    multiple files support

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import pandas as pd
 import re
 
-droppedfile = sys.argv[1]
+droppedfiles = sys.argv[1:]
 
 dropper = [
     "Caller", 
@@ -26,12 +26,14 @@ dropper = [
     "Error Message"
 ]
 
-s = str(droppedfile)
-res = re.sub(r"Call_Records_from_(\d{4}-\d{2}-\d{2})_\d{2}_\d{2}_\d{2}_to_.*$", r"CallRecords\1", s)
+for i in droppedfiles:
+    s = str(i)
+    res = re.sub(r"Call_Records_from_(\d{4}-\d{2}-\d{2})_\d{2}_\d{2}_\d{2}_to_.*$", r"CallRecords\1", s)
 
-df = pd.read_csv(droppedfile)
-df = df.drop(columns=dropper)
-df = df[df["Billing Prefix"] != "onnet_in"]
-df = df[df["Billed Duration, sec"] >= 30]
+    df = pd.read_csv(i)
+    df = df.drop(columns=dropper)
+    df = df[df["Billing Prefix"] != "onnet_in"]
+    df = df[df["Billed Duration, sec"] >= 30]
 
-df.to_csv(f"{res}.csv", index=False)
+    df.to_csv(f"{res}.csv", index=False)
+
